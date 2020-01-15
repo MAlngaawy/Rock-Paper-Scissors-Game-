7b161171cbc81c46152f75c6122800c2f9469602
malngaawy <mohager477@gmail.com>
2020-01-15 14:49:29 +0200

    show and hide overlay

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,12 @@
-console.log("it's work")
+
+// Let's Start Bitch
+// Define Variables
+const overlay = document.querySelector(".rules-overlay")
+
+// Show & Hide Overlay
+function displayOverlay() {
+  overlay.classList.remove("d-none")
+}
+function hideOverlay() {
+  overlay.classList.add("d-none")
+}
